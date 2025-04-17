8e3d021139637ea0c541cee19c91f5a5746462d0
fiverrguru360 <er.sarfarazkhan1@gmail.com>
2025-04-17 11:42:35 +0530

    Delete Src directory

--- Src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import FiverrGuru360 from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FiverrGuru360 />);
