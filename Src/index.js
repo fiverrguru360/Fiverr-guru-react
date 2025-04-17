35d234358147a66d92d477d7293661deca3c9ff3
fiverrguru360 <er.sarfarazkhan1@gmail.com>
2025-04-17 11:38:24 +0530

    Add files via upload

---
+++ Src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FiverrGuru360 from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FiverrGuru360 />);
